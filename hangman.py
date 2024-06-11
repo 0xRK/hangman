44e752c70e2d3508ed493d2583372b3a342c05f3
Rishi Krishna <rishi1.krishna1@gmail.com>
2024-06-11 17:09:43 -0500

    Word and Letter class implementation.

--- hangman.py
+++ hangman.py
@@ -1,5 +1,26 @@
 import random
 
+class Letter:
+  def __init__(self, value):
+    self.value = value
+    self.guessed = False
+
+class Word:
+  def __init__(self):
+    self.letters = []
+    self.solved = False
+
+  def print_word(self):
+    for letter in self.letters:
+      print(letter.value)
+
+# initial setup
+
 words = open('words.txt', 'r').readlines()
 r = random.randint(0, len(words) - 1)
-word = words[r]
+random_word = words[r]
+word = Word()
+
+for character in random_word:
+  letter = Letter(character)
+  word.letters.append(letter)
